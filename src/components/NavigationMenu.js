a2000c05ec6f5a2705056a83d450e397b9c4a72d
Paul Kim <manus.dream.language@gmail.com>
2022-05-21 13:35:30 +0900

    update

--- src/components/NavigationMenu.js
+++ src/components/NavigationMenu.js
@@ -2,66 +2,68 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 export default function NavigationMenu({ index, isSelected }) {
-  const navigate = useNavigate();
-  
-  const menuList = [
-    { title: '홈' },
-    { title: '랭킹' },
-    { title: '피드' },
-    { title: '채팅' },
-    { title: 'MY' },
-  ];
-  const currentMenu = menuList[index];
+    const navigate = useNavigate();
 
-  const moveToPageTop = () => {
-    window.scrollTo(0, 0);
-  };
+    const menuList = [
+        { title: "홈" },
+        { title: "랭킹" },
+        { title: "피드" },
+        { title: "채팅" },
+        { title: "MY" },
+    ];
+    const currentMenu = menuList[index];
 
-  const moveToPage = () => {
-    switch (index) {
-      case 0:
-        navigate('/');
-        return;
-      case 1:
-        navigate('/ranking');
-        return;
-      case 2:
-        navigate('/feed');
-        return;
-      case 3:
-        navigate('/chat');
-        return;
-      case 4:
-        navigate('/myPage');
-        return;
-      default:
-        navigate('/');
-        return;
-    }
-  };
+    const moveToPageTop = () => {
+        window.scrollTo(0, 0);
+    };
 
-  return (
-    <MenuItem
-      onClick={() => {
-        moveToPage();
-        moveToPageTop();
-      }}
-    >
-      <span style={isSelected ? {color: '#1A35D2'} : {}}>{currentMenu.title}</span>
-    </MenuItem>
-  );
+    const moveToPage = () => {
+        switch (index) {
+            case 0:
+                navigate("/");
+                return;
+            case 1:
+                navigate("/ranking");
+                return;
+            case 2:
+                navigate("/feed");
+                return;
+            case 3:
+                navigate("/chat/1/pol/wer");
+                return;
+            case 4:
+                navigate("/myPage");
+                return;
+            default:
+                navigate("/");
+                return;
+        }
+    };
+
+    return (
+        <MenuItem
+            onClick={() => {
+                moveToPage();
+                moveToPageTop();
+            }}
+        >
+            <span style={isSelected ? { color: "#1A35D2" } : {}}>
+                {currentMenu.title}
+            </span>
+        </MenuItem>
+    );
 }
 
 const MenuItem = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 20%;
-  height: 64px;
-  & span {
-    font-size: 16px;
-    line-height: 1.15;
-    text-align: center;
-    color: #DDDDDD;
-  }
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 20%;
+    height: 64px;
+    & span {
+        font-size: 16px;
+        line-height: 1.15;
+        text-align: center;
+        color: #dddddd;
+    }
 `;
