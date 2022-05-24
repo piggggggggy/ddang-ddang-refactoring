acb74c9daa3a5282862cbd13eb298e061f0ba211
diasm <diasm2@gmail.com>
2022-05-24 16:51:25 +0700

    change to axios feed in feedAction

--- src/store/thunk-actions/feedActions.js
+++ src/store/thunk-actions/feedActions.js
@@ -1,67 +1,61 @@
-import { feedActions } from "../slices/feedSlice";
-import axios from "axios";
+import { feedActions } from "../slices/feedSlice"
+import { api } from "../../modules/api"
 
 // 피드 전체 조회
 export const feedsLatestAxios = (region) => {
-    return async function (dispatch) {
-        await axios
-            .post("/api/feeds?type=latest", region)
-            .then((res) => {
-                console.log(res);
-                dispatch(feedActions.getFeed({ feeds: res.data.rows }));
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    };
-};
+  return async function (dispatch) {
+    await api
+      .post("/api/feeds?type=latest", region)
+      .then((res) => {
+        console.log(res)
+        dispatch(feedActions.getFeed({ feeds: res.data.rows }))
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+}
 export const feedsPopularityAxios = (region) => {
-    return async function (dispatch) {
-        await axios
-            .post(
-                "/api/feeds?type=popularity",
-                region
-            )
-            .then((res) => {
-                console.log(res);
-                dispatch(feedActions.getFeed({ feeds: res.data.rows }));
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    };
-};
+  return async function (dispatch) {
+    await api
+      .post("/api/feeds?type=popularity", region)
+      .then((res) => {
+        console.log(res)
+        dispatch(feedActions.getFeed({ feeds: res.data.rows }))
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+}
 export const feedsDistanceAxios = (region) => {
-    return async function (dispatch) {
-        await axios
-            .post("/api/feeds?type=distance", region)
-            .then((res) => {
-                console.log(res);
-                dispatch(feedActions.getFeed({ feeds: res.data.rows }));
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    };
-};
+  return async function (dispatch) {
+    await api
+      .post("/api/feeds?type=distance", region)
+      .then((res) => {
+        console.log(res)
+        dispatch(feedActions.getFeed({ feeds: res.data.rows }))
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+}
 
 export const writeCommentsAxios = (comment, token, feedId) => {
-    return async function (dispatch) {
-        await axios
-            .post(
-                `/api/feeds/${feedId}/comments`,
-                {
-                    comment: comment,
-                },
-                {
-                    headers: { Authorization: `Bearer ${token}` },
-                }
-            )
-            .then((res) => {
-                console.log(res);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    };
-};
+  return async function (dispatch) {
+    await api
+      .post(
+        `/api/feeds/${feedId}/comments`,
+        {
+          comment: comment,
+        },
+      )
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+}
