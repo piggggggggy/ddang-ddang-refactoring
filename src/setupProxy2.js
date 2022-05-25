0cd48c005ac761a50568988c9cf786a014445f5e
diasm <diasm2@gmail.com>
2022-05-25 09:55:22 +0700

    [fix]setupProxy 비활성화

--- src/setupProxy2.js
+++ src/setupProxy2.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     ["/api"], //자신이 설정한 업로드 API이름을 입력한다
     createProxyMiddleware({
-      //   target: process.env.BASE_URL,
+      //   target: env.BASE_URL,
       target: "https://thaitour.shop",
       changeOrigin: true,
     })
