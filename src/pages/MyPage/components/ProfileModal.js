607be2b500aa3acf56d6e51b74dd9fe5d4cc06db
steve_park <diasm2@gmail.com>
2022-05-28 20:40:02 +0700

    [delete]react-dotenv

--- src/pages/MyPage/components/ProfileModal.js
+++ src/pages/MyPage/components/ProfileModal.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
-import env from "react-dotenv";
 import axios from "axios";
 import ProfilePreview from "../elements/ProfilePreview";
 
