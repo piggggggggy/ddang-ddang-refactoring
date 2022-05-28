f1e1da1d373b3d7b76eec47d5f6df5d6e2903f6d
steve_park <diasm2@gmail.com>
2022-05-28 20:21:39 +0700

    [issue] geolocation 로딩이 늦는 문제

--- src/pages/Main/hooks/MainHooks.js
+++ src/pages/Main/hooks/MainHooks.js
@@ -10,10 +10,7 @@ const useMainData = () => {
     const [loading, setLoading] = useState(false);
     const [questList, setQuestList] = useState([]);
     const [questType, setQuestType] = useState("all");
-    const [location, setLocation] = useState({
-        lat: 0,
-        lng: 0,
-    });
+    const [location, setLocation] = useState({});
     const [region, setRegion] = useState({
         regionDong: "",
         regionGu: "",
@@ -24,80 +21,44 @@ const useMainData = () => {
         setLoading(true);
 
         navigator.geolocation.getCurrentPosition(async (res) => {
+            const { latitude, longitude } = res.coords;
             //사용자의 위치를 가져온다.
+            console.log(latitude, longitude);
             setLocation({
-                lat: res.coords.latitude,
-                lng: res.coords.longitude,
+                lat: latitude,
+                lng: longitude,
             });
 
-
-            // 만약에 사용자의 위치에서 시구동을 못가져오면 현재 가능한지 않는 지역에 있습니다.
+            console.log(location);
             const userdata = await KakaoService.getAddress({
                 location: location,
             });
-
             console.log(userdata);
-            // 주소가 아무것도 없다고 나오면 강제로 주소를 정해준다.
-            if (
-                typeof userdata.si == "undefined" &&
-                typeof userdata.gu == "undefined" &&
-                typeof userdata.dong == "undefined"
-            ) {
-                //이상한 주소
 
-                //강제로 설정한다.
-                const tempLocation = {
-                    lat: 37.5172363,
-                    lng: 127.0473248,
-                };
+            setRegion({
+                regionSi: userdata.si,
+                regionGu: userdata.gu,
+                regionDong: userdata.dong,
+            });
 
-                const data = await getQuestList({location: tempLocation});
+            const data = await getQuestList({ location: location });
 
-
-
-                if (data.rows.length > 0) {
-                    setQuestList(data.rows);
-                    dispatch(
-                        userActions.setQuest({
-                            list: data.rows,
-                            region: data.currentRegion,
-                        })
-                    );
-                }
-
-                // 현재 설정 저장 
-                setRegion(data.currentRegion);
-
-
-                // 경고 문구 반환
-                return alert("현재 가능한 지역이 아닙니다. 가상환경에서 시작합니다.");
-            } else {
-
-                //  퀘스트를 가져온다
-                const data = await getQuestList(
-                    res.coords.latitude,
-                    res.coords.longitude
+            if (data.rows.length > 0) {
+                setQuestList(data.rows);
+                dispatch(
+                    userActions.setQuest({
+                        list: data.rows,
+                        region: data.currentRegion,
+                    })
                 );
-
-                // 퀘스트가 있다면 퀘스트 리스트를 설정한다.
-                if (data.rows.length > 0) {
-                    setQuestList(data.rows);
-                    dispatch(
-                        userActions.setQuest({
-                            list: data.rows,
-                            region: data.currentRegion,
-                        })
-                    );
-                }
-                // 현재 지역 설정 
-                setRegion(data.currentRegion);
             }
-            console.log(userdata);
-
-            setTimeout(() => {
-                setLoading(false);
-            }, 200);
+            // 현재 지역 설정
+            setRegion(data.currentRegion);
         });
+
+        setTimeout(() => {
+            setLoading(false);
+        }, 200);
     }, []);
 
     return {
