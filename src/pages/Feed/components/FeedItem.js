f9c0690f638f47414fb9bddb8acd23f2cea2d4e9
Paul Kim <manus.dream.language@gmail.com>
2022-05-29 23:15:49 +0900

    final style for feed page

--- src/pages/Feed/components/FeedItem.js
+++ src/pages/Feed/components/FeedItem.js
@@ -124,67 +124,36 @@ export default function FeedItem(props) {
         console.log(commentArr);
     }, [commentArr]);
 
-    console.log(props);
     return (
-        <Grid
-            onClick={onClick}
-            mystyles=" box-shadow: 1px 1px 4px 1px rgba(155, 155, 155, 0.15);margin-left: -20px;"
-        >
+        <Feed {...props} onClick={onClick}>
+            <Text mystyles="position: absolute; left: -50px; top: 20px;font-weight: 700; font-size: 12px;">
+                {item.createdAt.substring(5, 10).replace("-", ".")}
+            </Text>
             <Grid
                 flex
                 direction="row"
-                mystyles="min-height: 78px; overflow-wrap: break-word;"
+                mystyles="min-height: 78px; padding: 15px 15px 0 15px; overflow-wrap: break-word;"
             >
-                <ColorBlock {...props}>
-                    <Text mystyles="margin-top: -40px; font-weight: 700; font-size: 12px;">
-                        {item.createdAt.substring(5, 10).replace("-", ".")}
+                <Grid mystyles="width: 1000px; overflow-wrap: break-word;">
+                    <Text mystyles="font-weight: 700; font-size: 16px; color: #A3D4FB;letter-spacing: -0.05em;">
+                        Title
                     </Text>
-                </ColorBlock>
-                <Grid flex direction="column">
-                    <Grid mystyles="width: 250px; overflow-wrap: break-word; border: 2px solid red;">
-                        <Grid flex justifyContent="space-between">
-                            <Text mystyles="font-weight: 700; font-size: 12px;letter-spacing: -0.05em;">
-                                Title
-                            </Text>
-                            <Grid
-                                flex
-                                alignItems="center"
-                                mystyles="margin-left: 14px;width: 100px;"
-                            >
-                                <IconButton
-                                    color={
-                                        likedByMe === true ? "error" : "primary"
-                                    }
-                                    aria-label="like"
-                                    onClick={likeHandler}
-                                >
-                                    <FavoriteIcon sx={{ height: "14px" }} />
-                                </IconButton>
-                                <Text mystyles="display:inline; font-size: 12px; font-weight: 600; margin-left: -10px; color: #A3D4FB;">
-                                    {counter}
-                                </Text>
-                                <IconButton
-                                    onClick={commentHandler}
-                                    color={"primary"}
-                                >
-                                    <MessageIcon sx={{ height: "14px" }} />
-                                </IconButton>
-                                <Text mystyles="display:inline; font-size: 12px; margin-left: -10px; color: #A3D4FB; font-weight: 600;">
-                                    {commentArr.length}
-                                </Text>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                    <Text mystyles="font-weight: 400; font-size: 8px; letter-spacing: -0.05em;">
+                    <Text mystyles="font-weight: 400; font-size: 12px; letter-spacing: -0.05em;">
                         {item.content}
                     </Text>
                 </Grid>
             </Grid>
-            <SecondBlock {...props} onClick={detailHandler}>
+            <Grid
+                flex
+                justifyContent="center"
+                alignItems="center"
+                mystyles="height: 15px; width: 50px; margin: auto; padding-right: 20px"
+                onClick={detailHandler}
+            >
                 <Text pointer mystyles="font-weight: 800">
                     ...
                 </Text>
-            </SecondBlock>
+            </Grid>
             <AnimatePresence>
                 {detailOpen && (
                     <Grid
@@ -194,7 +163,7 @@ export default function FeedItem(props) {
                         initial={{ y: -250, opacity: 0 }}
                         animate={{ y: 0, opacity: 1 }}
                         transition={{ delay: 0.1 }}
-                        mystyles="height: 100px;"
+                        mystyles="height: 100px"
                     >
                         {imagesArr.map((image, idx) => (
                             <Grid
@@ -210,7 +179,31 @@ export default function FeedItem(props) {
                     </Grid>
                 )}
             </AnimatePresence>
-
+            <Grid mystyles="height: 26px;">
+                <Grid
+                    flex
+                    justifyContent="center"
+                    alignItems="center"
+                    mystyles="width: 100px; height: 100%; margin-left: 14px;"
+                >
+                    <IconButton
+                        color={likedByMe === true ? "error" : "primary"}
+                        aria-label="like"
+                        onClick={likeHandler}
+                    >
+                        <FavoriteIcon sx={{ height: "14px" }} />
+                    </IconButton>
+                    <Text mystyles="display:inline; font-size: 12px; font-weight: 600; margin-left: -10px; color: #A3D4FB;">
+                        {counter}
+                    </Text>
+                    <IconButton onClick={commentHandler} color={"primary"}>
+                        <MessageIcon sx={{ height: "14px" }} />
+                    </IconButton>
+                    <Text mystyles="display:inline; font-size: 12px; margin-left: -10px; color: #A3D4FB; font-weight: 600;">
+                        {commentArr.length}
+                    </Text>
+                </Grid>
+            </Grid>
             <AnimatePresence>
                 {commentIsOpen && (
                     <>
@@ -225,21 +218,33 @@ export default function FeedItem(props) {
                                 <Text mystyles="font-size: 12px;">
                                     {comment?.comment}
                                 </Text>
-                                <Text mystyles="font-size: 12px;">
-                                    {comment?.updatedAt !== ""
-                                        ? comment?.updatedAt.substring(0, 10)
-                                        : comment?.createdAt.substring(0, 10)}
-                                </Text>
-                                {playerEmail === comment?.player?.email && (
-                                    <Button
-                                        mystyles="background: #F3AC9C; border-radius: 25px; border: none; box-shadow: 1px 1px 1px 1px #0B325E"
-                                        onClick={() => {
-                                            deleteComment(comment?.id);
-                                        }}
-                                    >
-                                        x
-                                    </Button>
-                                )}
+                                <Grid
+                                    flex
+                                    justifyContent="space-between"
+                                    mystyles="width: 100px;"
+                                >
+                                    <Text mystyles="font-size: 12px;">
+                                        {comment?.updatedAt !== ""
+                                            ? comment?.updatedAt.substring(
+                                                  0,
+                                                  10
+                                              )
+                                            : comment?.createdAt.substring(
+                                                  0,
+                                                  10
+                                              )}
+                                    </Text>
+                                    {playerEmail === comment?.player?.email && (
+                                        <Button
+                                            mystyles="border: none; background-color: white;"
+                                            onClick={() => {
+                                                deleteComment(comment?.id);
+                                            }}
+                                        >
+                                            x
+                                        </Button>
+                                    )}
+                                </Grid>
                             </Grid>
                         ))}
                         <Grid
@@ -259,31 +264,20 @@ export default function FeedItem(props) {
                     </>
                 )}
             </AnimatePresence>
-        </Grid>
+        </Feed>
     );
 }
 
-const ColorBlock = styled(motion.div)`
-    ${(props) => (props.page === 0 ? "background-color:  #F3AC9C" : "")};
-    ${(props) => (props.page === 1 ? "background-color:  #A3D4FB" : "")};
-    ${(props) => (props.page === 2 ? "background-color:  #EDEA50" : "")};
-    width: 100px;
-    height: 78px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`;
-
-const SecondBlock = styled(motion.div)`
-    ${(props) => (props.page === 0 ? "border-left: 64px solid  #F3AC9C" : "")};
-    ${(props) => (props.page === 1 ? "border-left: 64px solid #A3D4FB" : "")};
-    ${(props) => (props.page === 2 ? "border-left: 64px solid #EDEA50" : "")};
-    height: 15px;
-    width: 310px;
-    margin-left: -0.5px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+const Feed = styled(motion.li)`
+    ${(props) => (props.page === 0 ? "border-left: 70px solid #F3AC9C" : "")};
+    ${(props) => (props.page === 1 ? "border-left: 70px solid #A3D4FB" : "")};
+    ${(props) => (props.page === 2 ? "border-left: 70px solid #EDEA50" : "")};
+    min-height: 106px;
+    margin-top: 18px;
+    border-radius: 10px;
+    box-shadow: 1px 1px 1px 3px rgba(0, 0, 0, 0.05);
+    margin-left: -40px;
+    position: relative;
 `;
 
 // const createComment = async (comment) => {
