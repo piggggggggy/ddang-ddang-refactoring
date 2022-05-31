3075db6497689551439276d8c77814deaef9ace3
steve_park <diasm2@gmail.com>
2022-06-01 01:27:12 +0700

    [Fix] 스피너 예외처리

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import {
@@ -19,53 +20,56 @@ import Grid from "./elements/Grid";
 import "./App.css";
 
 function App() {
+    const path = window.location.pathname.split("/")[1];
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const [isLoading, setIsLoading] = React.useState(true);
 
     React.useEffect(() => {
-        //토큰확인
-        const refreshtoken = TokenService.getLocalRefreshToken();
-        const accesstoken = TokenService.getLocalAccessToken();
+        if (path !== "signin" && path !== "signup" && path !== "kakaoauth") {
+            //토큰확인
+            const refreshtoken = TokenService.getLocalRefreshToken();
+            const accesstoken = TokenService.getLocalAccessToken();
 
-        //둘다 없으면 로그인페이지로 이동
-        if (!refreshtoken && !accesstoken) {
-            alert("로그인이 필요합니다.");
-            navigate("/signin");
-            setTimeout(() => {
+            //둘다 없으면 로그인페이지로 이동
+            if (!refreshtoken && !accesstoken) {
+                alert("로그인이 필요합니다.");
+                navigate("/signin");
+                setTimeout(() => {
+                    setIsLoading(false);
+                }, 3000);
+            } else {
+                //
+                dispatch(loginCheckAxios(accesstoken));
                 setIsLoading(false);
-            }, 3000);
-        } else {
-            //
-            dispatch(loginCheckAxios(accesstoken));
-            setIsLoading(false);
+            }
         }
     }, []);
 
-    if (isLoading) {
-        return (
-            <Container>
-                {/* <Grid flex alignItems="center" justifyContent="center">
-                    <img
-                        src={loadingSpinnerGif}
-                        alt=""
-                        style={{ width: "50%", height: "50%" }}
-                    />
-                </Grid> */}
+    // if (isLoading) {
+    //     return (
+    //         <Container>
+    //             {/* <Grid flex alignItems="center" justifyContent="center">
+    //                 <img
+    //                     src={loadingSpinnerGif}
+    //                     alt=""
+    //                     style={{ width: "50%", height: "50%" }}
+    //                 />
+    //             </Grid> */}
 
-                <video
-                    width="100%"
-                    height="100%"
-                    preload="auto"
-                    muted
-                    autoPlay={true}
-                >
-                    <source src={splashmp4} type="video/mp4" />
-                    Your browser does not support HTML5 video.
-                </video>
-            </Container>
-        );
-    }
+    //             <video
+    //                 width="100%"
+    //                 height="100%"
+    //                 preload="auto"
+    //                 muted
+    //                 autoPlay={true}
+    //             >
+    //                 <source src={splashmp4} type="video/mp4" />
+    //                 Your browser does not support HTML5 video.
+    //             </video>
+    //         </Container>
+    //     );
+    // }
 
     return (
         <Container>
