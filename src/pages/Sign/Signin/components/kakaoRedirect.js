37748e4dc204b6e927f460630015ceafe50cba15
steve_park <diasm2@gmail.com>
2022-06-01 18:20:31 +0700

    [add]카카오 로그인

--- src/pages/Sign/Signin/components/kakaoRedirect.js
+++ src/pages/Sign/Signin/components/kakaoRedirect.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Container from "../../../../elements/Container";
 import splashmp4 from "../../../../assets/images/png/sign/NDND_splash.mp4";
 import TokenService from "../../../../services/token.service";
 import api from "../../../../modules/api";
 import axios from "axios";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { userActions } from "../../../../store/slices/userSlice";
 import Mbti from "../../../Sign/Signup/components/Mbti";
+import KakaoLogin from "./kakaologin";
 
-const OAuth2RedirectHandler = async (props) => {
+export default async function OAuth2RedirectHandler() {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
     const [isLoading, setIsLoading] = React.useState(true);
+    const [isMbti, setIsMbti] = React.useState(false);
     //파라미터에서 리프레쉬 값가져오기
 
     const kakaoLogin = async () => {
@@ -20,7 +25,7 @@ const OAuth2RedirectHandler = async (props) => {
         TokenService.setRefreshToken(code);
 
         try {
-            // 리프레쉬 토큰을 가지고 엑세스 토큰발행(로그인 과정)
+            //리프레쉬 토큰을 가지고 엑세스 토큰발행(로그인 과정)
             const response = await axios.get(
                 `${process.env.REACT_APP_BASE_URL}/api/players/auth/getToken`,
                 {
@@ -36,29 +41,48 @@ const OAuth2RedirectHandler = async (props) => {
             // console.log(response)
 
             // 가져온 엑세스 토큰을 로컬스토리지 저장
-            // const accesstoken = infodata.headers["Authorization"];
             const accesstoken = response.headers["accesstoken"];
-
             TokenService.setAccessToken(accesstoken);
 
+            // auth를 이용해 데이터를 가져온다.
             const infodata = await api.get("/api/players/auth");
+            console.log(infodata);
             const { mbti } = infodata.data.user;
 
-            if (mbti) {
-                //MBTI 입력해야함
-                const gotoMbti = () => navigate("/mbti");
+            dispatch(
+                userActions.signin({
+                    user: infodata.data.user,
+                    token: accesstoken,
+                })
+            );
+
+            navigate("/mbti");
+
+            if (mbti === "") {
+                console.log(mbti);
+                setIsMbti(true);
+                navigate("/mbti");
             }
-            setIsLoading(false);
+
+            console.log(isMbti);
+            setTimeout(() => {
+                setIsLoading(false);
+            }, 3000);
+
+            setIsLoading(true);
+            setTimeout(() => {
+                navigate("/");
+            }, 3000);
+
+            // setIsLoading(false);
         } catch (err) {
             console.log(err);
         }
-    };
 
-    //페이지 이동
-    React.useEffect(() => {
-        console.log("hello")
-        kakaoLogin();
-        // dispatch(googleLogin(code));
+        // // const moveToSignupPage = () => {
+        // //     navigate("/signup/1");
+        // };
+
         if (isLoading) {
             return (
                 <Container>
@@ -83,10 +107,16 @@ const OAuth2RedirectHandler = async (props) => {
                 </Container>
             );
         }
-        navigate("/");
+    };
+
+    React.useEffect(() => {
+        kakaoLogin();
+        // if (isMbti) {
+        //     console.log(isMbti);
+        //     navigate("/sign/signup");
+        // }
+        // console.log(isMbti);
     }, []);
 
     return <>hello world</>;
-};
-
-export default OAuth2RedirectHandler;
+}
